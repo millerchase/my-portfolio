91a7c006becbca41b19a4df53ebe790d92e57087
CMiller <mlr.chase40@gmail.com>
2022-08-11 20:38:00 -0500

    style touch up

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
